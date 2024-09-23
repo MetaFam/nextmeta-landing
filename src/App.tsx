c92d8e61edb6996b482de603ea6be075055b1ed6
innkeeping <innkeeping@metagame.wtf>
2024-09-23 18:41:05 -0500

    basic start with dependencies🕹

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
      <div className="card bg-base-100 w-96 shadow-xl">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
+      src="/assets/nextmeta-logo.webp"
+      alt="NextMeta" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title">NextMeta</h2>
+    <p>NextMeta is a podcast and a newsletter about onchain economies, coordination and the bigger picture.</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">Collect/Listen</button>
     </div>
   </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,50 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    daisyui,
-  ],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        light: {
+          "primary": "#00afaa",
+          "secondary": "#d30000",
+          "accent": "#00fb00",
+          "neutral": "#292929",
+          "base-100": "#e5ffff",
+          "info": "#00a4e0",
+          "success": "#00bb96",
+          "warning": "#ffb600",
+          "error": "#ff6294",
+        },
+        dark: {
+
+          "primary": "#d400ff",
+          "secondary": "#008aff",
+          "accent": "#0065ff",
+          "neutral": "#14110d",
+          "base-100": "#1d282f",
+          "info": "#35a0ff",
+          "success": "#9be500",
+          "warning": "#b14b00",
+          "error": "#ff5f78",
+        },
+      },
+    ],
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    prefix: "",
+    themeRoot: ":root",
+  },
+
+  //...
 }
+
 
